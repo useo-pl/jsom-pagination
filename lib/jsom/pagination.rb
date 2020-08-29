201f8b2eb09d76f78e1b20e81b9693a83310ff7e
Sebastian Wilgosz <sebastian@driggl.com>
2020-08-29 20:47:01 +0200

    Add metadata to the loaded files

--- lib/jsom/pagination.rb
+++ lib/jsom/pagination.rb
@@ -4,10 +4,10 @@ require 'jsom/pagination/version'
 require 'jsom/pagination/pagy'
 
 require 'jsom/pagination/page'
+require 'jsom/pagination/meta_data'
 
 module JSOM
   module Pagination
     class Error < StandardError; end
-    # Your code goes here...
   end
 end
